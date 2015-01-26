5ed91253d0e6283c3bc9989b7589f72f91073f72
DSuhinin <suhinin.dmitriy@gmail.com>
2015-01-26 15:41:46 +0200

    *added missing files - complete v1 functionality of phpbb extension

---
+++ phpbb/virgil/login/ucp/virgil_login_ucp_info.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package   	Virgil Login
+ * @copyright 	Copyright 2015 http://www.virgilsecurity.com - All rights reserved.
+ * @license
+ */
+namespace virgil\login\ucp;
+
+class virgil_login_ucp_info
+{
+	function module ()
+	{
+    }
+
+	function install()
+	{
+	}
+
+	function uninstall()
+	{
+	}
+}
---
+++ phpbb/virgil/login/ucp/virgil_login_ucp_module.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package   	Virgil Login
+ * @copyright 	Copyright 2015 http://www.virgilsecurity.com - All rights reserved.
+ * @license
+ */
+namespace virgil\login\ucp;
+
+if (! defined ('IN_PHPBB'))
+{
+	exit ();
+}
+
+class virgil_login_ucp_module
+{
+	// Add Social Link to UCP \ Profile \ Social link.
+	public function main ($id, $mode)
+	{
+
+	}
+}
