0f4913aa65beb72bcc772a452367ae770780d92f
DSuhinin <suhinin.dmitriy@gmail.com>
2015-01-29 19:08:39 +0200

    *added missing file

---
+++ wordpress/virgil-login/js/profile.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+    jQuery('.user-pass1-wrap').hide();
+    jQuery('.user-pass2-wrap').hide();
+
+    var virgil = '<tr class="stop-virgil"><th>&nbsp;</th><td>You are using Virgil Login. To stop using it and start use normal password, please click on this <a href="javascript:void(0);" id="show-new-password">link.</a></td></tr>';
+    jQuery('.user-description-wrap').after(virgil);
+
+    jQuery('#show-new-password').click(function() {
+        jQuery('#your-profile').append('<input type="hidden" name="stop-using-virgil" value="1" />');
+        jQuery('.user-pass1-wrap, .user-pass2-wrap').show();
+        jQuery('.stop-virgil').remove();
+    })
+})
