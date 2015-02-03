d398af5f4f26af22ebcb30bd2138ca21f43c3342
DSuhinin <suhinin.dmitriy@gmail.com>
2015-02-03 11:12:10 +0200

    *minor changes according to the profile logic for phpbb plugin

---
+++ phpbb/virgil/login/language/en/frontend.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package   	Virgil Login
+ * @copyright 	Copyright 2015 http://www.virgilsecurity.com - All rights reserved.
+ * @license
+ */
+
+if (! defined ('IN_PHPBB'))
+{
+	exit ();
+}
+
+if (empty ($lang) || ! is_array ($lang))
+{
+	$lang = array ();
+}
+
+// Virgil Login Frontend.
+$lang = array_merge ($lang, array (
+
+));
